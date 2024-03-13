ca98f026e5d7905eca0c217af4fb0f3a3853a01a
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-03-13 11:54:44 -0700

    files manager

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,8 @@ router.get("/connect", AuthController.getConnect);
 router.get("/disconnect", AuthController.getDisconnect);
 router.get("/users/me", UsersController.getMe);
 router.post("/files", FilesController.postUpload);
+router.get("/files", FilesController.getIndex);
+router.post("/files/:id", FilesController.getShow);
+router.put("/files/:id/publish", FilesController.putPublish);
+router.put("/files/:id/unpublish", FilesController.putUnpublish);
 module.exports = router;
